1507eb3ef0bdede8fbd9bf02c74c534e8ab6b697
seungohjin <corrobolea@gmail.com>
2024-11-12 18:41:25 +0900

    update: 홈 UI 작업(완)

--- app/src/main/java/com/mogun/planetcoffee/Home.kt
+++ app/src/main/java/com/mogun/planetcoffee/Home.kt
@@ -3,11 +3,16 @@ package com.mogun.planetcoffee
 data class Home(
     val user: User,
     val appbarImage: String,
-
-    )
+    val banner: Banner,
+)
 
 data class User(
     val nickname: String,
-    val startCount: Int,
+    val starCount: Int,
     val totalCount: Int,
+)
+
+data class Banner(
+    val image: String,
+    val contentDescription: String,
 )
--- app/src/main/java/com/mogun/planetcoffee/HomeFragment.kt
+++ app/src/main/java/com/mogun/planetcoffee/HomeFragment.kt
@@ -1,12 +1,13 @@
 package com.mogun.planetcoffee
 
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.mogun.planetcoffee.databinding.FragmentHomeBinding
 
-class HomeFragment: Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private lateinit var binding: FragmentHomeBinding
 
@@ -14,15 +15,85 @@ class HomeFragment: Fragment(R.layout.fragment_home) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentHomeBinding.bind(view)
 
-        val homeData = context?.readData() ?: return
-        binding.appBarTitleTextView.text = getString(R.string.appbar_title_text, homeData.user.nickname)
-        binding.starCountTextView.text = getString(R.string.appbar_start_title, homeData.user.startCount)
+        val homeData = context?.readData("home.json", Home::class.java) ?: return
+        val menuData = context?.readData("menu.json", Menu::class.java) ?: return
 
-        binding.progressBar.progress = homeData.user.startCount
+        initAppBar(homeData)
+        initRecommendMenuList(homeData, menuData)
+        initBanner(homeData)
+        initFoodMenuList(menuData)
+
+        binding.nestedScrollView.setOnScrollChangeListener { y, _, scrollY, _, oldScrollY ->
+            if (scrollY == 0) {
+                binding.floatingActionButton.extend()
+            } else {
+                binding.floatingActionButton.shrink()
+            }
+        }
+    }
+
+    private fun initFoodMenuList(menuData: Menu) {
+        binding.foodMenuList.titleTextView.text =
+            getString(R.string.food_menu_title)
+
+        menuData.food.forEach { menuItem ->
+            binding.foodMenuList.menuLayout.addView(
+                MenuView(context = requireContext()).apply {
+                    setTitle(menuItem.name)
+                    setImageUrl(menuItem.image)
+                }
+            )
+        }
+    }
+
+    private fun initBanner(homeData: Home) {
+        binding.bannerLayout.bannerImageView.apply {
+            Glide.with(this)
+                .load(homeData.banner.image)
+                .into(this)
+            this.contentDescription = homeData.banner.contentDescription
+        }
+    }
+
+    private fun initRecommendMenuList(
+        homeData: Home,
+        menuData: Menu
+    ) {
+        binding.recommandMenuList.titleTextView.text =
+            getString(R.string.recommend_title, homeData.user.nickname)
+
+        menuData.coffee.forEach { menuItem ->
+            binding.recommandMenuList.menuLayout.addView(
+                MenuView(context = requireContext()).apply {
+                    setTitle(menuItem.name)
+                    setImageUrl(menuItem.image)
+                }
+            )
+        }
+    }
+
+    private fun initAppBar(homeData: Home) {
+        binding.appBarTitleTextView.text =
+            getString(R.string.appbar_title_text, homeData.user.nickname)
+        binding.starCountTextView.text =
+            getString(
+                R.string.appbar_start_title,
+                homeData.user.starCount,
+                homeData.user.totalCount
+            )
+
         binding.progressBar.max = homeData.user.totalCount
 
         Glide.with(binding.appBarImageView)
             .load(homeData.appbarImage)
             .into(binding.appBarImageView)
+
+        ValueAnimator.ofInt(0, homeData.user.starCount).apply {
+            duration = 1000
+            addUpdateListener {
+                binding.progressBar.progress = it.animatedValue as Int
+            }
+            start()
+        }
     }
 }
